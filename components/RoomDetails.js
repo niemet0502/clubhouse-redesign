d3c5b62b6b4f943efae54a3b66c64c5bc080f71c
niemet0502 <vincentmarius8@gmail.com>
2021-04-03 19:14:01 +0200

    main :: created RoomDetails component

---
+++ components/RoomDetails.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const RoomDetails = (props) => {
+
+}
+
+const styles = StyleSheet.create({
+  
+})
