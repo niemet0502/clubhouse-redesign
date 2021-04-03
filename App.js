1e012208ee22e593b963ffe79bd2da4b612ed531
niemet0502 <vincentmarius8@gmail.com>
2021-04-03 19:09:59 +0200

    main :: input search and user image displayed

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Build Cluhouse Redesign!</Text>
+      <View style={styles.searchWrapper}>
+      <TextInput style={styles.input} placeholder={'Find people & clubs'}/>
+      <View style={styles.userWrapper}>
+        <Image
+          style={styles.userProfil}
+          source={require('./assets/user.jpeg')}
+        />
+      </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +23,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F6F1ED',
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20
   },
+  searchWrapper:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  input:{
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 220,
+    paddingLeft: 20,
+    borderRadius: 15
+  },
+  userProfil:{
+    width: 45,
+    height: 45,
+    borderRadius: 50
+  },
+  userWrapper:{
+    padding: 5,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    borderRadius: 15
+  }
 });
