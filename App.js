397980b279daa5570358b5f7f03ca15f62ed4ff6
niemet0502 <vincentmarius8@gmail.com>
2021-04-03 22:52:08 +0200

    main :: created Upcoming room view

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+const Upcoming = [
+  {
+    title: 'Startup club',
+    date: '10:30 PM',
+    description: 'UX for Startups: Design System'
+  },
+  {
+    title: 'Live & Clicking',
+    date: '10:30 PM',
+    description: 'How to make an impact online'
+  }
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Build Cluhouse Redesign!</Text>
       <View style={styles.searchWrapper}>
-      <TextInput style={styles.input} placeholder={'Find people & clubs'}/>
-      <View style={styles.userWrapper}>
-        <Image
-          style={styles.userProfil}
-          source={require('./assets/user.jpeg')}
-        />
+        <TextInput style={styles.input} placeholder={'Find people & clubs'}/>
+        <View style={styles.userWrapper}>
+          <Image
+            style={styles.userProfil}
+            source={require('./assets/user.jpeg')}
+          />
+        </View>
       </View>
+
+      <View style={styles.upcomingRoom}>
+        <View style={styles.roomContainer}>
+          <MaterialCommunityIcons name="home" size={24} color="#CEE7D9" />
+          <View style={styles.iconContainer}>
+            <View style={styles.roomTitle}>
+              <Text style={styles.title}>Startup club</Text>
+              <Text style={styles.roomTime}>10:30 PM</Text>
+            </View>
+            <Text style={styles.description}>UX for Startups: Design System</Text>
+          </View>
+        </View>
+
       </View>
     </View>
   );
@@ -24,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F6F1ED',
-    paddingTop: 50,
+    paddingTop: 70,
     paddingLeft: 20,
     paddingRight: 20
   },
@@ -50,5 +76,34 @@ const styles = StyleSheet.create({
     borderColor: '#C0C0C0',
     borderWidth: 1,
     borderRadius: 15
+  },
+  upcomingRoom:{
+    marginTop: 35,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    padding: 10
+  },
+  roomContainer:{
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  iconContainer: {
+    flex: 1,
+    paddingLeft: 7
+  },
+  roomTitle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    textTransform: 'uppercase',
+  },
+  description: {
+    marginTop: 5,
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
+  },
+  roomTime:{
+    color: '#C0C0C0'
   }
 });
