a434f65d6b751a841f2b50c1fa316e8c5a34ad96
niemet0502 <vincentmarius8@gmail.com>
2021-04-04 02:45:12 +0200

    main :: adde RoomDetails component structure

--- components/RoomDetails.js
+++ components/RoomDetails.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const RoomDetails = (props) => {
-
+  return(
+    <View>
+      <Text>I'm Room Details component</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
